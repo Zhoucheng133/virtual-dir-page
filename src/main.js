615c956ac35ed3171b8003efe781f2d8b4a6e541
zhoucheng <zhoucheng133@outlook.com>
2023-11-21 09:23:18 +0800

    添加v-body-scroll-lock

--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@ import App from './App.vue'
 
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
-import 'bootstrap-icons/font/bootstrap-icons.css'
-
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import VBodyScrollLock from 'v-body-scroll-lock';
 
 Vue.use(Antd);
+Vue.use(VBodyScrollLock)
 
 Vue.config.productionTip = false
 
