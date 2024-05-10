cbfcc57a88c673729a0669b954efc1196fd73b54
DESKTOP-AUU8MEV\周诚 <zhoucheng133@outlook.com>
2024-05-10 19:34:22 +0800

    迁移目录

--- README.md
+++ README.md
@@ -1,16 +1,3 @@
-# Virtual Directory (UI界面)
+# Virtual Directory 虚拟目录的UI页面
 
-## 简介
-
-![vue](https://img.shields.io/badge/vue.js-2.6.14-green?logo=vue.js)
-![Antd](https://img.shields.io/badge/Antd-1.7.8-blue)
-![axios](https://img.shields.io/badge/axios-^1.6.1-orange)
-![crypto-js](https://img.shields.io/badge/crypto_js-4.2.0-yellow)
-![element-ui](https://img.shields.io/badge/element_ui-2.15.14-lightblue)
-![plyr](https://img.shields.io/badge/plyr-3.7.8-red)
-![v-body-scroll-lock](https://img.shields.io/badge/v_body_scroll_lock-1.1.3-purple)
-![vue-contextmenujs](https://img.shields.io/badge/vue_contextmenujs-1.4.11-pink)
-
-![License](https://img.shields.io/badge/License-MIT-dark_green)
-
-**完整的项目[在这里](https://github.com/Zhoucheng133/virtual-directory)**
+Developed with Vue3 (JavaScript)
--- src/main.js
+++ src/main.js
@@ -1,26 +1,12 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-
+import { createPinia } from 'pinia'
 import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/antd.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import VBodyScrollLock from 'v-body-scroll-lock';
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-import VuePlyr from 'vue-plyr'
-import 'vue-plyr/dist/vue-plyr.css'
-import Contextmenu from "vue-contextmenujs"
+import 'ant-design-vue/dist/reset.css';
+import 'bootstrap-icons/font/bootstrap-icons.min.css';
+const pinia = createPinia();
+import VuePlyr from 'vue-plyr';
+import 'vue-plyr/dist/vue-plyr.css';
+import VueLazyLoad from 'vue3-lazyload'
 
-Vue.use(ElementUI);
-Vue.use(VuePlyr, {
-  plyr: {}
-})
-Vue.use(Antd);
-Vue.use(VBodyScrollLock)
-Vue.use(Contextmenu);
-
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+createApp(App).use(VuePlyr, {plyr: {}}).use(Antd).use(pinia).use(VueLazyLoad, {}).mount('#app');
